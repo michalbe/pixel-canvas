ce5ba62ee7260d2f777205d5300a0de661c3f1f7
Michał Budzyński <michal@virtualdesign.pl>
2016-04-13 10:13:05 +0200

    code cleanup

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 'use strict';
 /* global window, document */
+var a1 = Math.round(Math.random()*50);
+var b2 = Math.round(Math.random()*50);
+var a = Math.round(Math.random()*50);
+var b = Math.round(Math.random()*50);
+var c = Math.round(Math.random()*50);
+var d = Math.round(Math.random()*50);
+
 window.onload = function() {
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
@@ -11,20 +18,6 @@ window.onload = function() {
   //p(5, 5);
 
   var line = function(x0, y0, x1, y1) {
-    // var dx = x2 - x1;
-    // var dy = y2 - y1;
-    // for (var x = x1; x<x2; x++) {
-    //   var preY = y1 + dy * (x - x1) / dx;
-    //   var y = Math.round(preY);
-    //   var yDiff = y - preY;
-    //   if (yDiff > 0) {
-    //     //p(x, y - 1);
-    //   } else if (yDiff < 0) {
-    //     //p(x, y + 1);
-    //   }
-    //   console.log(x, y);
-    //   p(x, y);
-    // }
     var dx = Math.abs(x1-x0);
      var dy = Math.abs(y1-y0);
      var sx = (x0 < x1) ? 1 : -1;
@@ -43,10 +36,6 @@ window.onload = function() {
      }
   };
 
-  line(0, 0, 50, 25);
-  line(50, 25, 30, 40);
-  line(30, 40, 0, 0);
-
   // ctx.moveTo(0, 0);
   // ctx.lineTo(50, 25);
   // ctx.lineTo(30, 40);
@@ -55,7 +44,59 @@ window.onload = function() {
 
   var finalCanvas = document.getElementById('final-canvas');
   var finalCtx = finalCanvas.getContext('2d');
-  finalCtx.imageSmoothingEnabled = false;
-  finalCtx.mozImageSmoothingEnabled = false;
-  finalCtx.drawImage(canvas, 0, 0, 500, 500);
+
+  var time = 0;
+  // var center1X = Math.round(Math.random()*50);
+  // var center1Y = Math.round(Math.random()*50);
+  // var center2X = Math.round(Math.random()*50);
+  // var center2Y = Math.round(Math.random()*50);
+  // var center3X = Math.round(Math.random()*50);
+  // var center3Y = Math.round(Math.random()*50);
+  //
+  // var speed1 = Math.random();
+  // var speed2 = Math.random();
+  // var speed3 = Math.random();
+  // var newCoords = function(x, y, or1, or2, radius, speed) {
+  //   var origin = {
+  //     x: or1,
+  //     y: or2
+  //   };
+  //   return {
+  //     x: Math.round((Math.cos(time*speed) * radius + origin.x)),
+  //     y: Math.round((Math.sin(time*speed) * radius + origin.y))
+  //   };
+  // };
+
+  // var r1 = ~~(Math.random()*25);
+  // var r2 = ~~(Math.random()*25);
+  // var r3 = ~~(Math.random()*25);
+
+  var draw = function() {
+    time++;
+    canvas.width = canvas.width;
+    finalCanvas.width = finalCanvas.width;
+
+    // var nC = newCoords(a, b, center1X, center2Y, r1, speed1);
+    // a = nC.x;
+    // b = nC.y;
+    //
+    // nC = newCoords(a1, b2, center2X, center2Y, r2, speed2);
+    // a1 = nC.x;
+    // b2 = nC.y;
+    //
+    // nC = newCoords(c, d, center3X, center3Y, r3, speed3);
+    // c = nC.x;
+    // d = nC.y;
+
+    line(a1, b2, a, b);
+    line(a, b, c, d);
+    line(c, d, a1, b2);
+
+    finalCtx.imageSmoothingEnabled = false;
+    finalCtx.mozImageSmoothingEnabled = false;
+    finalCtx.drawImage(canvas, 0, 0, 500, 500);
+  };
+
+  // setInterval(draw, 1000/33);
+  draw();
 };
